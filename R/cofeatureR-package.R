e705f94140788e7fc805f07b50a15e79dc42e3af
Fong Chun Chan <fongchun@alumni.ubc.ca>
2018-02-15 20:47:29 +0000

    Fix spelling mistake

--- R/cofeatureR-package.R
+++ R/cofeatureR-package.R
@@ -1,7 +1,7 @@
 #' cofeatureR: Generate Cofeature Matrices
 #'
 #' Generate cofeature (feature by sample) matrices. The package 
-#' utilizies ggplot2::geom_tile to generate the matrix allowing for easy
+#' utilizes ggplot2::geom_tile to generate the matrix allowing for easy
 #' customization of additions from the base matrix.
 #' 
 #' @docType package
